6d8dba003d5f06f996718802a763c32360b3e1ff
Aghakarim-23 <hemidzade1010@gmail.com>
2026-01-14 21:11:13 +0400

    Add login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,45 @@
+import Link from "next/link"
+
 export default function Login() {
   return (
-    <div>Login</div>
+     <div className="min-h-screen flex justify-center items-center px-4 bg-gray-100">
+
+      <div className="bg-white max-w-md w-full p-6 rounded-md shadow-md">
+
+        <h2 className="text-center text-2xl font-bold mb-2">Login</h2>
+
+        <form className="flex flex-col gap-3  rounded-md">
+
+          <div>
+            <label htmlFor="email" className="block mb-1">Email</label>
+            <input type="email" name="email" id="email" placeholder="Enter your email" className="pl-2 rounded-md h-12 border border-gray-400 w-full "/>
+          </div>
+
+          <div> 
+            <label htmlFor="password" className="block mb-1">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter your password" className="pl-2 rounded-md h-12 border border-gray-400 w-full "/>
+          </div>
+
+          <div className="my-3">
+            <button className="w-full rounded-md bg-blue-500 hover:bg-blue-600 transition active:bg-blue-700 h-12 text-white cursor-pointer">Login</button>
+          </div>
+
+        </form>
+
+        
+
+        <div className="text-center">
+          <span>Don't have an account ?</span>
+          <Link href={"/register"} className="text-blue-600 hover:underline"> Register</Link>
+        </div>
+
+        <div className="text-center mt-3">
+          <Link href={"/forgot-password"} className="text-blue-600 hover:underline"> Forgot password ?</Link>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
